21e369142f00008de0587ec0ebfa29493411c03b
Anders Petersson <mr.petersson@gmail.com>
2016-09-06 20:41:27 +0200

    no message

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,33 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var theLabel: UILabel!
     
     @IBOutlet weak var text1: UITextField!
     
     @IBOutlet weak var text2: UITextField!
     
-
+    
     @IBAction func buttonTapped(_ sender: AnyObject) {
-        print(text1.text!)
-        print(text2.text!)
-        theLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
-
+        
+        var addition = true
+        
+        if addition {
+            theLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        } else {
+            theLabel.text = "Answer: \(Double(text1.text!)! - Double(text2.text!)!)"
+        }
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
